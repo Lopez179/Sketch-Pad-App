b2d56633af634ea9bc0e76c2374a2cb91dd8aef3
Leander <lflopez@ualberta.ca>
2023-02-20 16:36:59 -0700

    stroke erasing implemented, must solve error message

--- SnapNotes/assets.py
+++ SnapNotes/assets.py
@@ -12,7 +12,7 @@ class PointLocation:
         return str((self.x,self.y))
     
     def link_stroke(self,stroke_address):
-        self.linked_strokes.append = stroke_address
+        self.linked_strokes.append(stroke_address)
 
 
 #The implementation of Board could cause a bug where data isn't saved properly
@@ -20,7 +20,7 @@ class Board:
     def __init__(self, x_size,y_size):
         self.x_size = x_size
         self.y_size = y_size
-
+        self.stroke_list = []
         self.points = []
         for i in range(self.y_size):
             for j in range(self.x_size):
@@ -33,19 +33,30 @@ class Board:
                 row.append(self.points[j])
             print(row)
 
-    def index(self,x,y):
+    def get_point(self, x, y):
         return self.points[y*self.x_size + x]
+
+    def link_point(self, x, y, stroke_adress):
+        self.points[y*self.x_size + x].link_stroke(stroke_adress)
+    
+    def delete_stroke(self, stroke_address):
+        stroke_address.erase_coverage()
+        self.stroke_list.remove(stroke_address)
+    
+
         
 
 class Stroke:
-    def __init__(self, colour):
+    def __init__(self, colour, Userspace):
+        self.Userspace = Userspace
         self.colour = colour
         self.coverage = []
+    #Userspace is only defined in main.py, this will not work if the canvas is not named Userspace
+    def erase_coverage(self):
+        for i in self.coverage:
+            self.Userspace.delete(i)
+
 
 if __name__ == "__main__":
     Userboard = Board(20,20)
-    #Userboard.print_board()
-    print(Userboard.index(19,19))
-    print(Userboard.index(0,0))
-    print(Userboard.index(17,15))
     
--- SnapNotes/main.py
+++ SnapNotes/main.py
@@ -17,8 +17,7 @@ root = tk.Tk()
 root.geometry(run_size)
 root.title("Sketch v0")
 
-#Get Program Working
-Userboard = Board(run_width,run_height)
+
 
 #--------------Basic Drawing Functions---------------------
 
@@ -26,7 +25,8 @@ Userboard = Board(run_width,run_height)
 def graphite(stroke, point, colour):
     x = point[0]
     y = point[1]
-    Userspace.create_rectangle(x,y,x,y,fill=colour)
+    stroke.coverage.append(Userspace.create_rectangle(x,y,x,y,fill=colour))
+    Userboard.link_point(x,y, stroke)
 
 # uses graphite() to colour an small area
 def round_point(stroke, center, radius, colour="black"):
@@ -43,19 +43,20 @@ def pencil_tip(stroke, center,radius,colour="black"):
 
 #--------------Event Handling---------------------------
 # global variables are used so that events can share data
+Userboard = Board(run_width,run_height)
 global last_point
 global pencil_mode
 global stroke_record
-stroke_record = []
+stroke_record = Userboard.stroke_list
 pencil_mode = "draw"
 
 
-def on_stroke(event):
+def on_hold_down(event):
     global last_point
     global stroke_record
-    if pencil_mode == "draw":
 
-        x, y = event.x, event.y
+    x, y = event.x, event.y
+    if pencil_mode == "draw":
         
         displacement = (x - last_point[0], y - last_point[1])
         displacement_magnitude_square = displacement[0]**2 + displacement[1]**2
@@ -69,18 +70,24 @@ def on_stroke(event):
 
         pencil_tip(stroke_record[-1], (x,y), pencil_size)
         last_point = (x,y)
+    elif pencil_mode == "erase":
+        for i in Userboard.get_point(x,y).linked_strokes:
+            Userboard.delete_stroke(i)
 
 
 def on_tap(event):
     global last_point
     global stroke_record
+    x, y = event.x, event.y
     if pencil_mode == "draw":
         #create a stroke object
-        stroke_record.append(Stroke("black"))
+        stroke_record.append(Stroke("black", Userspace))
 
-        x, y = event.x, event.y
+        
         last_point = (x,y)
         round_point(stroke_record[-1], (x,y), pencil_size)
+
+
 
 
 def on_release(event):
@@ -94,9 +101,11 @@ def on_erase():
     global pencil_mode
     pencil_mode = "erase"
 
+
 def on_pencil():
     global pencil_mode
     pencil_mode = "draw"
+    print(stroke_record)
 
 
 Userspace = tk.Canvas(root, height=(run_height) - 30, width=run_width)
@@ -107,7 +116,7 @@ EraserButton.grid(row=0,column=0)
 PencilButton = tk.Button(root, text="Pencil", font=("Arieal", 10), command=on_pencil)
 PencilButton.grid(row=0,column=1)
 
-Userspace.bind('<B1-Motion>', on_stroke)
+Userspace.bind('<B1-Motion>', on_hold_down)
 Userspace.bind('<Button-1>', on_tap)
 Userspace.bind('<ButtonRelease-1>', on_release)
 
